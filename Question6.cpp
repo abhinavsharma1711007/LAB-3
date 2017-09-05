e883b43122187edf29dbafd3c830ed0a71002a18
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-09-05 14:36:08 +0530

    Create Question6.cpp

---
+++ Question6.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+int main(){
+int year;
+cout<< "Enter the year \t ";
+cin>> year;
+if (year%400==0)
+{
+   cout<< year << "\t" << "is not a leap year.";
+}
+else
+{
+   if(year%4==0)
+   {
+       cout<< year << "\t" << "is a leap year.";
+   }
+   else
+   {
+        cout<< year << "\t" << "is not a leap year.";
+
+   }
+}
+return 0;
+}
