12f98560fb4afc1dfd34af9584e2e42354acf722
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-09-05 14:52:19 +0530

    Update Question1.cpp

--- Question1.cpp
+++ Question1.cpp
@@ -5,16 +5,10 @@ float a,b;
 cout<< "Enter any two numbers";
 cin >>a >> b;
 if(a>b)
-{
-cout<<a <<">"<<b;
-}
+    cout<<a <<">"<<b;
 else if (b>a)
-{
-cout<<b<<">"<<a;
-}
+    cout<<b<<">"<<a;
 else
-{
-cout <<a <<"="<<b;
-}
+    cout <<a <<"="<<b;
 return 0;
 }
