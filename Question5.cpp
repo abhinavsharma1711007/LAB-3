b6db13edc349b86f8131a498142140ea7e2e8f17
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-09-05 14:35:29 +0530

    Create Question5.cpp

---
+++ Question5.cpp
@@ -0,0 +1,17 @@
+//Write a C++ program to check whether a number is even or odd.
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cout<< "Enter a number \t ";
+cin>> a;
+if(a%2==0)
+{
+   cout<< "Number is even.";
+}
+else
+{
+   cout<< "Number is odd.";
+}
+return 0;
+}
