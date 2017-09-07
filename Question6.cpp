89af9a52564f523738223521baa9681ab108b271
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-09-07 13:13:08 +0530

    Update Question6.cpp

--- Question6.cpp
+++ Question6.cpp
@@ -1,23 +1,16 @@
+#include <iostream>
 using namespace std;
-int main(){
-int year;
-cout<< "Enter the year \t ";
-cin>> year;
-if (year%400==0)
-{
-   cout<< year << "\t" << "is not a leap year.";
-}
-else
-{
-   if(year%4==0)
-   {
-       cout<< year << "\t" << "is a leap year.";
-   }
-   else
-   {
-        cout<< year << "\t" << "is not a leap year.";
-
-   }
-}
-return 0;
+int main()
+{ int year;
+  cout<< "Enter the year \t ";
+  cin>> year;
+  if (year%400==0)
+    cout<< year << "\t" << "is not a leap year.";
+  else
+  { if(year%4==0)
+     cout<< year << "\t" << "is a leap year.";
+    else
+     cout<< year << "\t" << "is not a leap year.":
+  }
+  return 0;
 }
