41e00ef30b6f138d55b2cc126aac4e1287accfbb
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-09-07 13:09:17 +0530

    Update Question4.cpp

--- Question4.cpp
+++ Question4.cpp
@@ -1,23 +1,18 @@
 #include<iostream>
 using namespace std;
-int main(){
-int a;
-cout<< "Enter any number  ";
-cin >>a;
-if(a%55==0)
-{
-cout << "No. is divisible by both 5 and 11";
-}
-else if ((a%5!=0 && a%11!=0))
-{
-cout <<" No. is not divisible by 5 or 11";
-}
-else{
-if(a%5==0)
-{
-cout <<" No. is divisible by 5";
-}
-if(a%11==0)
-{
-cout <<" No. is divisible by 11";
+int main()
+{ int a;
+  cout<< "Enter any number  ";
+  cin >>a;
+  if(a%55==0)
+    cout << "Number is divisible by both 5 and 11";
+  else if ((a%5!=0 && a%11!=0))
+    cout <<"Number is not divisible by 5 or 11";
+  else
+  { if(a%5==0)
+      cout <<"Number is divisible by 5";
+    if(a%11==0)
+      cout <<"Number is divisible by 11";
+  }
+  return 0 ;
 }
