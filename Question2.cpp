16e25634e088f62a58cfed2cc92653e7f2089660
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-09-07 13:03:38 +0530

    Update Question2.cpp

--- Question2.cpp
+++ Question2.cpp
@@ -1,20 +1,14 @@
 #include<iostream>
 using namespace std;
-int main(){
-float a,b,c;
-cout<< "Enter any three numbers";
-cin >>a >> b >>c;
-if((a>b)&&(a>c))
-{
-cout <<a;
-}
-else if (b>c)
-{
-cout<<b;
-}
-else
-{
-cout <<c;
-}
-return 0;
+int main()
+{ float a,b,c;
+  cout<< "Enter any three numbers";
+  cin >>a >> b >>c;
+  if((a>b)&&(a>c))
+    cout << a <<" is maximum number.";
+  else if (b>c)
+    cout << b <<" is maximum number.";
+  else
+    cout << c <<" is maximum number.";
+  return 0;
 }  
