2967b7813e334b67370febd0179bfdeac252993a
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-09-07 13:05:12 +0530

    Update Question3.cpp

--- Question3.cpp
+++ Question3.cpp
@@ -1,20 +1,14 @@
 #include<iostream>
 using namespace std;
-int main(){
-float a,b;
-cout<< "Enter any number  ";
-cin >>a;
-if(a>0)
-{
-cout<< "Positive Number";
-}
-else if (a<0)
-{
-cout<<"Negative Number";
-}
-else
-{
-cout <<"Zero";
-}
+int main()
+{ float a,b;
+  cout<< "Enter any number  ";
+  cin >>a;
+  if(a>0)
+    cout<< "Positive Number";
+  else if (a<0)
+    cout<<"Negative Number";
+  else
+    cout <<"Zero";
 return 0;
 }
