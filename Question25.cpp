a426b5a974dda53d4d997960d62a92046e8b1826
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-09-12 14:49:59 +0530

    Create Question25.cpp

---
+++ Question25.cpp
@@ -0,0 +1,11 @@
+//Write a C++ program to print all even numbers between 1 to 100. - using while loop
+#include<iostream>
+using namespace std;
+int main()
+{ int n = 0;
+  while(n<=98)
+  { n+=2;
+    cout << n << "\n";
+  }
+  return 0;
+}
