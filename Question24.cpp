ccbdc36f9ead5209c5a6881ce21fbabf0e10116b
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-09-12 14:49:21 +0530

    Create Question24.cpp

---
+++ Question24.cpp
@@ -0,0 +1,11 @@
+//Write a C++ program to print all alphabets from a to z. - using while loop
+#include<iostream>
+using namespace std;
+int main()
+{ char a = 'a';
+  while(a<='z')
+  { cout << a << "\n";
+    a++;
+  }
+  return 0;
+}
